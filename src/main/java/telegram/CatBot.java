69d13f5ebc10b1b84102291bc8a266df9146a83c
Nastya <vnedostupa@yandex.ru>
2023-11-25 16:57:44 +0300

    Connected with TheCatApi to get random cat pictures

--- src/main/java/telegram/CatBot.java
+++ src/main/java/telegram/CatBot.java
@@ -1,5 +1,4 @@
 package telegram;
-
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
@@ -10,9 +9,17 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMar
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -28,6 +35,10 @@ public class CatBot extends TelegramLongPollingBot {
     }
     private static String botToken = properties.getProperty("BOT_TOKEN");
     private static String botName = properties.getProperty("BOT_NAME");
+
+    private static String CAT_API_KEY = properties.getProperty("API_KEY");
+    private static final String CAT_API_URL = "https://api.thecatapi.com/v1/images/search";
+
 
     // Storage storage;
     @Override
@@ -50,7 +61,7 @@ public class CatBot extends TelegramLongPollingBot {
             if (text.equals("/start")) {
                 sendStartMessage(chatId);
             } else if (text.equals("Показать котят")) {
-                sendCatPhoto(chatId);
+                sendRandomCatPhoto(chatId);
             }
         }
     }
@@ -85,14 +96,33 @@ public class CatBot extends TelegramLongPollingBot {
         }
     }
 
-    private void sendCatPhoto(String chatId) {
-        SendPhoto sendPhoto = new SendPhoto();
-        sendPhoto.setChatId(chatId);
-        sendPhoto.setPhoto(new InputFile("https://img.goodfon.ru/wallpaper/nbig/3/3c/vzgliad-temnyi-fon-kotenok-kotiata-kotionok-regdoll-mnogo-ch.jpg"));
+    private void sendRandomCatPhoto(String chatId) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(CAT_API_URL)
+                .addHeader("x-api-key", CAT_API_KEY)
+                .get()
+                .build();
 
         try {
-            execute(sendPhoto);
-        } catch (TelegramApiException e) {
+            Response response = client.newCall(request).execute();
+            String jsonData = response.body().string();
+            JsonParser parser = new JsonParser();
+            JsonObject jsonObject = parser.parse(jsonData).getAsJsonArray().get(0).getAsJsonObject();
+            String imageUrl = jsonObject.get("url").getAsString();
+            Request imageRequest = new Request.Builder().url(imageUrl).build();
+            Response imageResponse = client.newCall(imageRequest).execute();
+            InputStream inputStream = imageResponse.body().byteStream();
+            File tempFile = File.createTempFile("temp", ".jpg");
+            Files.copy(inputStream, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+            SendPhoto sendPhoto = new SendPhoto();
+            sendPhoto.setChatId(chatId);
+            sendPhoto.setPhoto(new InputFile(tempFile));
+
+            execute(sendPhoto); // Отправка фото пользователю
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
